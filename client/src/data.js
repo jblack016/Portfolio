bcf14f0b12b0e76b7551267605f615831b02a7aa
jblack016 <j.black16@icloud.com>
2022-11-08 21:54:54 -0600

    updated commit

---
+++ client/src/data.js
@@ -0,0 +1,18 @@
+
+
+export const projects = [
+    {
+        title: "Entertainment Tracker",
+        subtitle: "MERN Stack",
+        description: "",
+        image: "",
+        link: "",
+    },
+    {
+        title: "Event Planner",
+        subtitle: "Java Stack",
+        description: "",
+        image: "",
+        link: "",
+    },
+]
