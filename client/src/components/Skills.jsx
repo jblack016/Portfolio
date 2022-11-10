60bd698ec3da853e61bb297c8dccce115c98974a
jblack016 <j.black16@icloud.com>
2022-11-09 22:33:21 -0600

    Updated Commit

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {BadgeCheckIcon, ChipIcon} from '@heroicons/react/solid'
+import {skills} from "../data"
 
 const Skills = () => {
   return (
@@ -16,7 +18,7 @@ const Skills = () => {
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          {Skills.map((skill) => (
+          {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                 <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
--- client/src/data.js
+++ client/src/data.js
@@ -3,16 +3,31 @@
 export const projects = [
     {
         title: "Entertainment Tracker",
-        subtitle: "MERN Stack",
-        description: "",
+        subtitle: "React Front-end",
+        description: "A responsive entertainment web app that allows users to browse and search through popular movies and TV Series.",
         image: "",
         link: "",
     },
     {
         title: "Event Planner",
-        subtitle: "Java Stack",
-        description: "",
+        subtitle: "Java Spring Full Stack",
+        description: "Site created for event planning that allows users to create and share events as well as joining and keeping track of the guest lists for the events.",
         image: "",
         link: "",
     },
 ]
+
+export const skills = [
+    "JavaScript",
+    "React",
+    "Python",
+    "Flask",
+    "Java",
+    "Spring",
+    "MySQL",
+    "MongoDB",
+    "HTML",
+    "CSS",
+    "Bootstrap",
+    "Material UI",
+];
