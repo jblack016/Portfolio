05a7fcb3d01c819f24fceeb228d38ec7bec693a6
jblack016 <j.black16@icloud.com>
2022-11-10 21:29:39 -0600

    Updated Commit

--- client/src/data.js
+++ client/src/data.js
@@ -5,15 +5,15 @@ export const projects = [
         title: "Entertainment Tracker",
         subtitle: "React Front-end",
         description: "A responsive entertainment web app that allows users to browse and search through popular movies and TV Series.",
-        image: "",
-        link: "",
+        image: "./giphy.mp4",
+        link: "https://github.com/jblack016/EntertainmentTracker.git",
     },
     {
         title: "Event Planner",
         subtitle: "Java Spring Full Stack",
         description: "Site created for event planning that allows users to create and share events as well as joining and keeping track of the guest lists for the events.",
-        image: "",
-        link: "",
+        image: "./giphy-2.mp4",
+        link: "https://github.com/jblack016/EventPlanner.git",
     },
 ]
 
